90755b3437bb0f5656a5d62b07663503ca554af3
burbage stacey <rstacey06@gmail.com>
2019-10-18 17:30:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,15 +1,11 @@
+  
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and i am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
-
-def introduction_with_language_optional(name, language = "ruby") 
-  puts "Hi, my name is #{name} and i am learning to program in #{language}"
-end
-
 
 
 
